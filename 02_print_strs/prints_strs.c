bf4f0edcdc0e3e0afdfe90671a0ddbeb14202f9b
Sergio Izquierdo <sergio.izqui@gmail.com>
2024-06-27 17:18:16 +0200

    Modificaciones Ejercicio 2

--- 02_print_strs/prints_strs.c
+++ 02_print_strs/prints_strs.c
@@ -38,6 +38,7 @@ int main(int argc,char **argv){
                         }
                         tamanoArgumento--;
                     }
+                    quitarEspacios=1;
                     write(1,"\n",1);
                 }
                 numArgc++;
